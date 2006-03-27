0f013e77e3a6b2eeb794f7ff01ac26334f2e6347
Tim Ellison <tellison@apache.org>
2006-03-27 20:24:18 +0000

    Test case for HARMONY-50 (rightly part of revision 389232)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/classlib/trunk@389257 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/luni/src/test/java/org/apache/harmony/tests/java/io/RandomAccessFileTest.java
@@ -0,0 +1,41 @@
+/* Copyright 2006 The Apache Software Foundation or its licensors, as applicable
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.harmony.tests.java.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import junit.framework.TestCase;
+  
+public class RandomAccessFileTest extends TestCase {
+
+	/**
+	 * @tests java.io.RandomAccessFile#RandomAccessFile(java.io.File, java.lang.String)
+	 */
+	public void test_ConstructorLjava_io_FileLjava_lang_String() throws IOException {
+		// Regression for HARMONY-50
+        File f = File.createTempFile("xxx", "yyy");
+        f.deleteOnExit();
+        RandomAccessFile raf = new RandomAccessFile(f, "rws");
+        raf.close();
+
+        f = File.createTempFile("xxx", "yyy");
+        f.deleteOnExit();
+        raf = new RandomAccessFile(f, "rwd");
+        raf.close();            
+    }
+}
