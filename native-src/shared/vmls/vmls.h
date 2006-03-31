78f6a24da4721d52c1144dad9cc042e0ec8efbc7
Tim Ellison <tellison@apache.org>
2006-03-31 15:24:54 +0000

    Add a reference implementation of VM local storage.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/classlib/trunk@390442 13f79535-47bb-0310-9956-ffa450edef68

---
+++ native-src/shared/vmls/vmls.h
@@ -0,0 +1,26 @@
+/* Copyright 2005, 2006 The Apache Software Foundation or its licensors, as applicable
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef vmls_h
+#define vmls_h
+
+void   initializeVMLocalStorage PROTOTYPE((JavaVM * vm));
+void   freeVMLocalStorage PROTOTYPE((JavaVM * vm));
+UDATA  JNICALL HyVMLSAllocKeys PROTOTYPE((JNIEnv * env, UDATA * pInitCount, ...));
+void * JNICALL HyVMLSSet PROTOTYPE((JNIEnv * env, void ** pKey, void * value));
+void * JNICALL HyVMLSGet PROTOTYPE((JNIEnv * env, void * key));
+void   JNICALL HyVMLSFreeKeys PROTOTYPE((JNIEnv * env, UDATA * pInitCount, ...));
+
+#endif     /* vmls_h */
