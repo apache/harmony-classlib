26c575d12dc7a95050c433f7e0578fe659262edd
George Harley <gharley@apache.org>
2006-04-27 19:42:00 +0000

    Merge all logging tests into one package whose name meets naming convention. Other modules to follow.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/classlib/trunk@397621 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/logging/src/test/java/org/apache/harmony/logging/tests/java/util/logging/ErrorManagerTest.java
@@ -0,0 +1,72 @@
+/* Copyright 2004 The Apache Software Foundation or its licensors, as applicable
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.harmony.logging.tests.java.util.logging;
+
+import java.util.logging.ErrorManager;
+
+import junit.framework.TestCase;
+
+/**
+ */
+public class ErrorManagerTest extends TestCase {
+	private static String className = ErrorManagerTest.class.getName();
+
+	ErrorManager em;
+
+	/*
+	 * @see TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+		em = new ErrorManager();
+	}
+
+	/*
+	 * @see TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/**
+	 * Constructor for ErrorManagerTest.
+	 * 
+	 * @param arg0
+	 */
+	public ErrorManagerTest(String arg0) {
+		super(arg0);
+	}
+
+	public void testErrorWithNullParameter() {
+		em.error(null, null, -1);
+		em.error(null, null, -2);
+	}
+
+	public void testError() {
+		em.error("test message, pls ignore it", new RuntimeException(
+				"test exception, pls ignore it"), ErrorManager.CLOSE_FAILURE);
+	}
+
+	public void testConstValue() {
+		assertEquals(3, ErrorManager.CLOSE_FAILURE);
+		assertEquals(2, ErrorManager.FLUSH_FAILURE);
+		assertEquals(5, ErrorManager.FORMAT_FAILURE);
+		assertEquals(0, ErrorManager.GENERIC_FAILURE);
+		assertEquals(4, ErrorManager.OPEN_FAILURE);
+		assertEquals(1, ErrorManager.WRITE_FAILURE);
+	}
+
+}
