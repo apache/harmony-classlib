7bdc0ccf36347fa4b86da6608fb5c7dbcc9fdcaa
Mikhail Loenko <mloenko@apache.org>
2006-05-26 05:17:25 +0000

    added a comment
    git-svn-id: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/classlib/trunk@409558 13f79535-47bb-0310-9956-ffa450edef68

--- modules/crypto/src/main/java/javax/crypto/Cipher.java
+++ modules/crypto/src/main/java/javax/crypto/Cipher.java
@@ -355,6 +355,9 @@ public class Cipher {
      */
     public final void init(int opmode, Key key) throws InvalidKeyException {
         if (sec_rand == null) {
+            // In theory it might be therad-unsafe but in the given case it's OK
+            // since it does not matter which SecureRandom instance is passed
+            // to the init()
             sec_rand = new SecureRandom();
         }
         init(opmode, key, sec_rand);
