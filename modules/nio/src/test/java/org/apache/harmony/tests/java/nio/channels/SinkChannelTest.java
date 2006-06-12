c637f080f664912d972f4297058f0bf20b1edca7
George Harley <gharley@apache.org>
2006-06-12 17:22:28 +0000

    Update unit test that is behaving in an unstable manner on Linux only.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/classlib/trunk@413712 13f79535-47bb-0310-9956-ffa450edef68

--- modules/nio/src/test/java/org/apache/harmony/tests/java/nio/channels/SinkChannelTest.java
+++ modules/nio/src/test/java/org/apache/harmony/tests/java/nio/channels/SinkChannelTest.java
@@ -103,35 +103,36 @@ public class SinkChannelTest extends TestCase {
 	 */
 	public void test_write_LByteBuffer_mutliThread() throws IOException,
 	        InterruptedException {
-		final int THREAD_NUM = 20;
-		Thread[] thread = new Thread[THREAD_NUM];
-		for (int i = 0; i < THREAD_NUM; i++) {
-			thread[i] = new Thread() {
-				public void run() {
-					try {
-						sink
-						        .write(ByteBuffer.wrap("bytes"
-						                .getBytes(ISO8859_1)));
-					} catch (IOException e) {
-						// ignore
-					}
-				}
-			};
-		}
-		for (int i = 0; i < THREAD_NUM; i++) {
-			thread[i].start();
-		}
-		for (int i = 0; i < THREAD_NUM; i++) {
-			thread[i].join();
-		}
-		ByteBuffer readBuf = ByteBuffer.allocate(THREAD_NUM * BUFFER_SIZE);
-		source.read(readBuf);
-		StringBuffer buf = new StringBuffer();
-		for (int i = 0; i < THREAD_NUM; i++) {
-			buf.append("bytes");
-		}
-		String readString = buf.toString();
-		assertEquals(readString, new String(readBuf.array(), ISO8859_1));
+        final int THREAD_NUM = 20;
+        Thread[] thread = new Thread[THREAD_NUM];
+        for (int i = 0; i < THREAD_NUM; i++) {
+            thread[i] = new Thread() {
+                public void run() {
+                    try {
+                        sink
+                                .write(ByteBuffer.wrap("bytes"
+                                        .getBytes(ISO8859_1)));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            };
+        }
+        for (int i = 0; i < THREAD_NUM; i++) {
+            thread[i].start();
+        }
+        for (int i = 0; i < THREAD_NUM; i++) {
+            thread[i].join();
+        }
+        ByteBuffer readBuf = ByteBuffer.allocate(THREAD_NUM * BUFFER_SIZE);
+        long readVal = source.read(readBuf);
+        StringBuffer buf = new StringBuffer();
+        for (int i = 0; i < THREAD_NUM; i++) {
+            buf.append("bytes");
+        }
+        String readString = buf.toString();
+        assertEquals(readString.length(), readVal);
+        assertEquals(readString, new String(readBuf.array(), ISO8859_1));
 	}
 
 	/**
