194a5e236d1dbef91fd96481b6f701811e397da8
Geir Magnusson Jr <geirm@apache.org>
2006-07-19 00:07:34 +0000

    move from o.a.h.s.provider.jsse to o.a.h.xnet.provider.jsse and also add the right patternset for the jar
    git-svn-id: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/classlib/trunk@423300 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/x-net/src/main/java/org/apache/harmony/xnet/provider/jsse/DataStream.java
@@ -0,0 +1,46 @@
+/*
+ *  Copyright 2006 The Apache Software Foundation or its licensors, as applicable.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/**
+ * @author Alexander Y. Kleymenov
+ * @version $Revision$
+ */
+
+package org.apache.harmony.xnet.provider.jsse;
+
+/**
+ * This interface represents the ability of the
+ * classes to provide the chunks of data.
+ */
+public interface DataStream {
+
+    /**
+     * Checks if there is data to be read.
+     * @return true if there is the input data in the stream,
+     * false otherwise
+     */
+    public boolean hasData();
+
+    /**
+     * Retrieves the data of specified length from the stream.
+     * If the data size in the stream is less than specified length,
+     * method returns all the data contained in the stream.
+     * @return byte array containing the demanded data.
+     */
+    public byte[] getData(int length);
+
+}
+
