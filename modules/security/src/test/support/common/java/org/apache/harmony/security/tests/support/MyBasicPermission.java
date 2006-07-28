67c86ce42f8b7379912850b8968434852fd04a21
Stepan Mishura <smishura@apache.org>
2006-07-28 07:59:50 +0000

    Adding serialVersionUID
    git-svn-id: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/classlib/trunk@426425 13f79535-47bb-0310-9956-ffa450edef68

--- modules/security/src/test/support/common/java/org/apache/harmony/security/tests/support/MyBasicPermission.java
+++ modules/security/src/test/support/common/java/org/apache/harmony/security/tests/support/MyBasicPermission.java
@@ -20,6 +20,8 @@ import java.security.BasicPermission;
 
 public class MyBasicPermission extends BasicPermission {
 
+    private static final long serialVersionUID = -4220730623258019258L;
+
     public MyBasicPermission(String name) {
         super(name);
     }
