6f8677c5f4e18ab8a97e2b06c714e8eeb4e7566f
Tim Ellison <tellison@apache.org>
2006-08-10 09:38:54 +0000

    Create a suncompat module.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/classlib/trunk@430337 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/suncompat/src/main/java/sun/misc/Base64Encoder.java
@@ -0,0 +1,27 @@
+/* Copyright 2006 The Apache Software Foundation or its licensors, as applicable
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package sun.misc;
+
+import java.io.UnsupportedEncodingException;
+
+import org.apache.harmony.luni.util.Base64;
+
+public class Base64Encoder {
+
+    public static String encode(byte[] in, String charsetName) throws UnsupportedEncodingException {
+        return Base64.encode(in, charsetName);
+    }
+}
