e0de393ae0422798c148d5c4fad9640dab1340f6
Mikhail Loenko <mloenko@apache.org>
2006-08-14 10:00:14 +0000

    applied patch for HARMONY-1159 [classlib][security] crypto provider: SHA1withDSA implementation
    git-svn-id: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/classlib/trunk@431317 13f79535-47bb-0310-9956-ffa450edef68

--- modules/security/src/main/java/common/org/apache/harmony/security/provider/crypto/CryptoProvider.java
+++ modules/security/src/main/java/common/org/apache/harmony/security/provider/crypto/CryptoProvider.java
@@ -14,51 +14,71 @@
  *  limitations under the License.
  */
 
-
 package org.apache.harmony.security.provider.crypto;
-
 
 import java.security.Provider;
 import java.security.AccessController;
 
+import org.apache.harmony.security.provider.crypto.RandomBitsSupplier;
 
 /**
- * Implementation of Provider for MessageDigest
+ * Implementation of Provider for SecureRandom, MessageDigest and Signature
  * using a Secure Hash Algorithm, SHA-1;
- * see SECURE HASH STANDARD, FIPS PUB 180-2
- * (http://csrc.nist.gov/publications/fips/fips180-2/fips180-2.pdf) <BR>
+ * see SECURE HASH STANDARD, FIPS PUB 180-1 (http://www.itl.nist.gov/fipspubs/fip180-1.htm) <BR>
  * <BR>
- * The implementation supports "SHA-1" algorithms described in
+ * The implementation supports "SHA1PRNG", "SHA-1" and "SHA1withDSA" algorithms described in
  * JavaTM Cryptography Architecture, API Specification & Reference
  */
 
-
 public final class CryptoProvider extends Provider {
-
 
     /**
      * Creates a Provider and puts parameters
      */
     public CryptoProvider() {
 
-        super("Crypto", 1.0, "HARMONY CryptoProvider");
+        super("Crypto", 1.0,
+                "HARMONY (SHA1 digest; SecureRandom; SHA1withDSA signature)");
 
-    //  names of classes implementing services
-    final String MD_NAME = 
-                 "org.apache.harmony.security.provider.crypto.SHA1_MessageDigestImpl";
+        //  names of classes implementing services
+        final String MD_NAME = "org.apache.harmony.security.provider.crypto.SHA1_MessageDigestImpl";
+        final String SR_NAME = "org.apache.harmony.security.provider.crypto.SHA1PRNG_SecureRandomImpl";
 
-        AccessController.doPrivileged( 
-            new java.security.PrivilegedAction() {
-                public Object run() {
+        final String SIGN_NAME = "org.apache.harmony.security.provider.crypto.SHA1withDSA_SignatureImpl";
 
-                    put("MessageDigest.SHA-1", MD_NAME);
-                    put("MessageDigest.SHA-1 ImplementedIn", "Software");
-                    put("Alg.Alias.MessageDigest.SHA1", "SHA-1");
-                    put("Alg.Alias.MessageDigest.SHA",  "SHA-1");
+        final String SIGN_ALIAS = "SHA1withDSA";
 
-                    return null;
+        AccessController.doPrivileged(new java.security.PrivilegedAction() {
+
+            public Object run() {
+
+                put("MessageDigest.SHA-1", MD_NAME);
+                put("MessageDigest.SHA-1 ImplementedIn", "Software");
+                put("Alg.Alias.MessageDigest.SHA1", "SHA-1");
+                put("Alg.Alias.MessageDigest.SHA", "SHA-1");
+
+                if (RandomBitsSupplier.isServiceAvailable()) {
+                    put("SecureRandom.SHA1PRNG", SR_NAME);
+                    put("SecureRandom.SHA1PRNG ImplementedIn", "Software");
                 }
+
+                put("Signature.SHA1withDSA", SIGN_NAME);
+                put("Signature.SHA1withDSA ImplementedIn", "Software");
+                put("Alg.Alias.Signature.SHAwithDSA", SIGN_ALIAS);
+                put("Alg.Alias.Signature.DSAwithSHA1", SIGN_ALIAS);
+                put("Alg.Alias.Signature.SHA1/DSA", SIGN_ALIAS);
+                put("Alg.Alias.Signature.SHA/DSA", SIGN_ALIAS);
+                put("Alg.Alias.Signature.SHA-1/DSA", SIGN_ALIAS);
+                put("Alg.Alias.Signature.DSA", SIGN_ALIAS);
+                put("Alg.Alias.Signature.DSS", SIGN_ALIAS);
+
+                put("Alg.Alias.Signature.OID.1.2.840.10040.4.3", SIGN_ALIAS);
+                put("Alg.Alias.Signature.1.2.840.10040.4.3", SIGN_ALIAS);
+                put("Alg.Alias.Signature.1.3.14.3.2.13", SIGN_ALIAS);
+                put("Alg.Alias.Signature.1.3.14.3.2.27", SIGN_ALIAS);
+
+                return null;
             }
-        );
+        });
     }
 }
