2b04e0b177bd414a5d3b36ee871a39dedf778fcc
Mark Hindess <hindessm@apache.org>
2006-10-03 08:56:31 +0000

    Migrate unix fetch-awt-depends.sh behaviour to make/depends.xml (in preparation for the windows equivalent [hopefully] being placed in the same file).
    git-svn-id: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/classlib/trunk@452353 13f79535-47bb-0310-9956-ffa450edef68

--- depends/libs/build/fetch-awt-depends.sh
+++ depends/libs/build/fetch-awt-depends.sh
@@ -1,87 +1,14 @@
 #!/bin/sh
 
-for d in depends make modules ;do
-  if [ \! -d $d ]; then
-    echo no $d directory
-    echo this script should be run from classlib top-level directory
-    echo
-    echo sh depends/libs/build/fetch-awt-swing.sh
-    exit 1
-  fi
-done
-
-LCMS_HOME=/usr
-mkdir -p depends/libs/build/lcms
-while read s t 
-do
-  src=$LCMS_HOME/$s
-  tgt=depends/libs/build/lcms/$t
-  if [ \! -e $src ]; then
-    echo $src not found
-    echo liblcms development package not install
-    echo for debian/ubuntu try: apt-get install liblcms1-dev
-    exit 1
-  fi
-  if [ -e $tgt ]; then
-    echo $tgt exists
-  else
-    echo Linking $src to $tgt
-    ln -s $src $tgt
-  fi
-done <<EOF
-  lib/liblcms.a liblcms.ia32
-  include/icc34.h icc34.h
-  include/lcms.h lcms.h
-EOF
-
-PNG_HOME=/usr
-mkdir -p depends/libs/build/png
-while read s t 
-do
-  src=$PNG_HOME/$s
-  tgt=depends/libs/build/png/$t
-  if [ \! -e $src ]; then
-    echo $src not found
-    echo libpng development package not install
-    echo for debian/ubuntu try: apt-get install libpng12-dev
-    exit 1
-  fi
-  if [ -e $tgt ]; then
-    echo $tgt exists
-  else
-    echo Linking $src to $tgt
-    ln -s $src $tgt
-  fi
-done <<EOF
-  lib/libpng.a libpng.ia32
-  include/pngconf.h pngconf.h
-  include/png.h png.h
-EOF
-
-JPEG_HOME=/usr
-mkdir -p depends/libs/build/jpeg
-while read s t 
-do
-  src=$JPEG_HOME/$s
-  tgt=depends/libs/build/jpeg/$t
-  if [ \! -e $src ]; then
-    echo $src not found
-    echo libjpeg development package not install
-    echo for debian/ubuntu try: apt-get install libjpeg62-dev
-    exit 1
-  fi
-  if [ -e $tgt ]; then
-    echo $tgt exists
-  else
-    echo Linking $src to $tgt
-    ln -s $src $tgt
-  fi
-done <<EOF
-  lib/libjpeg.a libjpeg.ia32
-  include/jconfig.h jconfig.lnx
-  include/jpeglib.h jpeglib.h
-  include/jmorecfg.h jmorecfg.h
-  include/jerror.h jerror.h
-EOF
-
+echo 
+echo '****************************************************************'
+echo 
+echo $0
+echo
+echo \"ant fetch-depends\" has been updated and this script is now
+echo obsolete. \ It is no longer necessary to run this script.
+echo 
+echo '****************************************************************'
+echo 
+sleep 3
 exit
