5758fa59ed45473d620353c6cd84c35b57597e03
Nathan Beyer <ndbeyer@apache.org>
2006-10-27 03:03:09 +0000

    Add explicit serialVersionUID fields, add missing annotations and type variables, clean up test case (proper assert usage)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/classlib/trunk@468246 13f79535-47bb-0310-9956-ffa450edef68

--- modules/print/src/test/api/java/common/javax/print/DocFlavorTest.java
+++ modules/print/src/test/api/java/common/javax/print/DocFlavorTest.java
@@ -14,112 +14,79 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-/** 
- * @author Irina A. Arkhipets 
- * @version $Revision: 1.3 $ 
- */ 
-
-/*
- * DocFlavorTest.java
- * 
- * JUnit tests for javax.print.DocFlavor class.
- * 
- */
 
 package javax.print;
 
 import junit.framework.TestCase;
 
 public class DocFlavorTest extends TestCase {
-
-static public void testStatic() {
-    System.out.println("----------------------------------------");
-    System.out.println("Static variables testing...");
-    try {
+    public void testStatic() {
         String encoding = System.getProperty("file.encoding");
-        if ((encoding != null) && (encoding.length() > 0)) {
-            assertEquals(DocFlavor.hostEncoding, encoding);
-        } else {
-            System.out.println(
-                    "WARNING: Can not get system file.encoding property!");
+        assertEquals(encoding, DocFlavor.hostEncoding);
+    }
+
+    public void testHashCode() {
+        DocFlavor f1 = new DocFlavor("text/plain; charset=us-ascii", "[B");
+        DocFlavor f2 = new DocFlavor("text/plain; charset=us-ascii (comments)", "[B");
+        assertEquals(f1.hashCode(), f1.hashCode());
+        assertEquals(f1.hashCode(), f2.hashCode());
+    }
+
+    public void testMyDocFlavor() {
+        tryNullPointer(null, "[B");
+        tryNullPointer("text/plain", null);
+    }
+
+    public void testEqualsObject() {
+        DocFlavor f1 = new DocFlavor("text/plain; charset=us-ascii", "[B");
+        DocFlavor f2 = new DocFlavor("text/plain; charset=us-ascii (comments)", "[B");
+        DocFlavor f3 = new DocFlavor("image/gif", "[B");
+        DocFlavor f4 = new DocFlavor("text/plain", "[B");
+        DocFlavor f5 = new DocFlavor("text/plain; charset=us-ascii", "[C");
+        assertEquals(f1, f1);
+        assertEquals(f1, f2);
+        assertFalse(f1.equals(f3));
+        assertFalse(f1.equals(f4));
+        assertFalse(f1.equals(f5));
+    }
+
+    public void testGetMediaSubtype() {
+        DocFlavor f = new DocFlavor("text/plain; charset=us-ascii", "[B");
+        assertEquals("plain",f.getMediaSubtype());
+    }
+
+    public void testGetMediaType() {
+        DocFlavor f = new DocFlavor("text/plain; charset=us-ascii", "[B");
+        assertEquals("text", f.getMediaType());
+    }
+
+    public void testGetMimeType() {
+        DocFlavor f = new DocFlavor("TEXT/plain; BBB=par1; aaa=par2 (comments)", "[B");
+        assertEquals("text/plain; aaa=\"par2\"; bbb=\"par1\"", f.getMimeType());
+    }
+
+    public void testGetParameter() {
+        DocFlavor f = new DocFlavor("TEXT/plain; BBB=par1; aaa=par2 (comments)", "[B");
+        assertEquals("par1", f.getParameter("bbb"));
+        assertNull(f.getParameter("absent"));
+    }
+
+    public void testGetRepresentationClassName() {
+        DocFlavor f = new DocFlavor("TEXT/plain; BBB=par1; aaa=par2 (comments)", "[B");
+        assertEquals("[B", f.getRepresentationClassName());
+    }
+
+    public void testToString() {
+        DocFlavor f = new DocFlavor("TEXT/plain; BBB=par1; aaa=par2 (comments)", "[B");
+        assertEquals("text/plain; aaa=\"par2\"; bbb=\"par1\"; class=\"[B\"", f.toString());
+    }
+
+    void tryNullPointer(String s1, String s2) {
+        try {
+            new DocFlavor(s1, s2);
+            fail();
+        } catch (NullPointerException e) {
+            /* NullPointerException is expected here, so the test passes */
         }
-    } catch (Exception e) {
-        System.out.println(
-                "WARNING: Can not get system file.encoding property!");
-        e.printStackTrace();
     }
-}
-
-public void testHashCode() {
-    DocFlavor f1 = new DocFlavor("text/plain; charset=us-ascii", "[B");
-    DocFlavor f2 = 
-            new DocFlavor("text/plain; charset=us-ascii (comments)", "[B");
-    assertEquals(f1.hashCode(), f1.hashCode());
-    assertEquals(f1.hashCode(), f2.hashCode());
-}
-
-public void testMyDocFlavor() {
-    tryNullPointer(null, "[B");
-    tryNullPointer("text/plain", null);
-}
-
-public void testEqualsObject() {
-    DocFlavor f1 = new DocFlavor("text/plain; charset=us-ascii", "[B");
-    DocFlavor f2 = 
-            new DocFlavor("text/plain; charset=us-ascii (comments)", "[B");
-    DocFlavor f3 = new DocFlavor("image/gif", "[B");
-    DocFlavor f4 = new DocFlavor("text/plain", "[B");
-    DocFlavor f5 = new DocFlavor("text/plain; charset=us-ascii", "[C");
-    assertTrue(f1.equals(f1));
-    assertTrue(f1.equals(f2));
-    assertFalse(f1.equals(f3));
-    assertFalse(f1.equals(f4));
-    assertFalse(f1.equals(f5));
-}
-
-public void testGetMediaSubtype() {
-    DocFlavor f = new DocFlavor("text/plain; charset=us-ascii", "[B");
-    assertTrue(f.getMediaSubtype().equals(new String("plain")));
-}
-
-public void testGetMediaType() {
-    DocFlavor f = new DocFlavor("text/plain; charset=us-ascii", "[B");
-    assertTrue(f.getMediaType().equals(new String("text")));
-}
-
-public void testGetMimeType() {
-    DocFlavor f = 
-            new DocFlavor("TEXT/plain; BBB=par1; aaa=par2 (comments)", "[B");
-    assertTrue((f.getMimeType()).equals(
-            new String("text/plain; aaa=\"par2\"; bbb=\"par1\"")));
-}
-
-public void testGetParameter() {
-    DocFlavor f =
-            new DocFlavor("TEXT/plain; BBB=par1; aaa=par2 (comments)", "[B");
-    assertEquals(f.getParameter("bbb"), new String("par1"));
-    assertNull(f.getParameter("absent"));
-}
-
-public void testGetRepresentationClassName() {
-    DocFlavor f = 
-            new DocFlavor("TEXT/plain; BBB=par1; aaa=par2 (comments)", "[B");
-    assertEquals(f.getRepresentationClassName(), new String("[B"));
-}
-
-public void testToString() {
-    DocFlavor f = 
-            new DocFlavor("TEXT/plain; BBB=par1; aaa=par2 (comments)", "[B");
-    assertEquals(f.toString(), new String(
-            "text/plain; aaa=\"par2\"; bbb=\"par1\"; class=\"[B\""));
-}
-
-void tryNullPointer(String s1, String s2) {
-    try {
-        new DocFlavor(s1, s2);
-        fail();
-    } catch (NullPointerException e) {
-        /* NullPointerException is expected here, so the test passes */
-    }
-}
 }
