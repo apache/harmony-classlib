4cb9a1707a09fff84e4073c58cf7f8f2122ba672
Nathan Beyer <ndbeyer@apache.org>
2006-10-29 06:13:32 +0000

    Cleanup ActivationGroupDesc and add RI-based test cases to assert correctness.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/classlib/trunk@468837 13f79535-47bb-0310-9956-ffa450edef68

--- modules/rmi/src/main/java/java/rmi/activation/ActivationGroupDesc.java
+++ modules/rmi/src/main/java/java/rmi/activation/ActivationGroupDesc.java
@@ -16,67 +16,49 @@
  * limitations under the License.
  */
 
-/**
- * @author  Victor A. Martynov
- * @version $Revision: 1.9.2.2 $
- */
 package java.rmi.activation;
 
 import java.io.Serializable;
 import java.rmi.MarshalledObject;
+import java.util.Arrays;
 import java.util.Properties;
 
-
-/**
- * @com.intel.drl.spec_ref
- *
- * @author  Victor A. Martynov
- * @version $Revision: 1.9.2.2 $
- */
 public final class ActivationGroupDesc implements Serializable {
-
     private static final long serialVersionUID = -4936225423168276595L;
 
     /**
      * The group's fully package qualified class name.
      */
-    String className;
+    private String className;
 
     /**
      * The location from where to load the group's class.
      */
-    String location;
+    private String location;
 
     /**
      * The group's initialization data.
      */
-    MarshalledObject data;
+    private MarshalledObject data;
 
     /**
      * The controlling options for executing the VM in another process.
      */
-    ActivationGroupDesc.CommandEnvironment env;
+    private ActivationGroupDesc.CommandEnvironment env;
 
     /**
-     * A properties map which will override those set by default in the subprocess environment
+     * A properties map which will override those set by default in the
+     * subprocess environment
      */
-    Properties props;
+    private Properties props;
 
-    /**
-     * @com.intel.drl.spec_ref
-     */
-    public ActivationGroupDesc(Properties props,
-            ActivationGroupDesc.CommandEnvironment env) {
+    public ActivationGroupDesc(Properties props, ActivationGroupDesc.CommandEnvironment env) {
         this(null, null, null, props, env);
     }
 
-    /**
-     * @com.intel.drl.spec_ref
-     */
-    public ActivationGroupDesc(String className, String codebase,
-            MarshalledObject data, Properties props,
-            ActivationGroupDesc.CommandEnvironment env) {
-
+    public ActivationGroupDesc(String className, String codebase, MarshalledObject data,
+            Properties props, ActivationGroupDesc.CommandEnvironment env) {
+        super();
         this.className = className;
         this.location = codebase;
         this.data = data;
@@ -84,141 +66,119 @@ public final class ActivationGroupDesc implements Serializable {
         this.env = env;
     }
 
-    /**
-     * @com.intel.drl.spec_ref
-     */
     public String getClassName() {
         return className;
     }
 
-    /**
-     * @com.intel.drl.spec_ref
-     */
     public String getLocation() {
         return location;
     }
 
-    /**
-     * @com.intel.drl.spec_ref
-     */
     public MarshalledObject getData() {
         return data;
     }
 
-    /**
-     * @com.intel.drl.spec_ref
-     */
     public ActivationGroupDesc.CommandEnvironment getCommandEnvironment() {
         return env;
     }
 
-    /**
-     * @com.intel.drl.spec_ref
-     */
     public Properties getPropertyOverrides() {
-        if (props == null) {
-            return null;
-        } else {
-            return (Properties) props.clone();
-        }
+        return props == null ? null : (Properties) props.clone();
     }
 
-    /**
-     * @com.intel.drl.spec_ref
-     */
-    public boolean equals(Object obj) {
-        if (obj instanceof ActivationGroupDesc) {
-            ActivationGroupDesc obj_agd = (ActivationGroupDesc) obj;
-            boolean p0, p1, p2, p3, p4, p5;
-
-            p0 = (location == null) ? obj_agd.location == null : location
-                    .equals(obj_agd.location);
-            p1 = (className == null) ? obj_agd.className == null : className
-                    .equals(obj_agd.className);
-            p2 = (data == null) ? obj_agd.data == null : data
-                    .equals(obj_agd.data);
-            p3 = (env == null) ? obj_agd.env == null : env.equals(obj_agd.env);
-            p4 = (props == null) ? obj_agd.props == null : props
-                    .equals(obj_agd.props);
-
-            return p0 & p1 & p2 & p3 & p4;
-        }
-        return false;
-    }
-
-    /**
-     * @com.intel.drl.spec_ref
-     */
+    @Override
     public int hashCode() {
-        int h0 = (className == null) ? 0 : className.hashCode();
-        int h1 = (location == null) ? 0 : location.hashCode();
-        int h2 = (data == null) ? 0 : data.hashCode();
-        int h3 = (props == null) ? 0 : props.hashCode();
-
-        return h0 ^ h1 ^ h2 ^ h3;
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((className == null) ? 0 : className.hashCode());
+        result = prime * result + ((data == null) ? 0 : data.hashCode());
+        result = prime * result + ((env == null) ? 0 : env.hashCode());
+        result = prime * result + ((location == null) ? 0 : location.hashCode());
+        result = prime * result + ((props == null) ? 0 : props.hashCode());
+        return result;
     }
 
-    /**
-     * @com.intel.drl.spec_ref
-     */
-    public static class CommandEnvironment implements Serializable {
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof ActivationGroupDesc)) {
+            return false;
+        }
+        final ActivationGroupDesc that = (ActivationGroupDesc) obj;
+        if (!(className == null ? that.className == null : className.equals(that.className))) {
+            return false;
+        }
+        if (!(data == null ? that.data == null : data.equals(that.data))) {
+            return false;
+        }
+        if (!(env == null ? that.env == null : env.equals(that.env))) {
+            return false;
+        }
+        if (!(location == null ? that.location == null : location.equals(that.location))) {
+            return false;
+        }
+        return (props == null ? that.props == null : props.equals(that.props));
+    }
 
+    public static class CommandEnvironment implements Serializable {
         private static final long serialVersionUID = 6165754737887770191L;
 
-        private String command = null;
+        private String command;
 
-        private String options[] = null;
+        private String options[];
 
-        /**
-         * @com.intel.drl.spec_ref
-         */
         public CommandEnvironment(String command, String[] options) {
+            super();
             this.command = command;
-            this.options = options;
+            if (options == null) {
+                this.options = null;
+            } else {
+                this.options = new String[options.length];
+                System.arraycopy(options, 0, this.options, 0, options.length);
+            }
         }
 
-        /**
-         * @com.intel.drl.spec_ref
-         */
         public String[] getCommandOptions() {
-            if (this.options == null) {
+            if (options == null) {
                 return new String[0];
             }
-            return this.options;
+            return options.clone();
         }
 
-        /**
-         * @com.intel.drl.spec_ref
-         */
         public String getCommandPath() {
             return this.command;
         }
 
-        /**
-         * @com.intel.drl.spec_ref
-         */
-        public boolean equals(Object obj) {
-            if (obj instanceof CommandEnvironment) {
-                CommandEnvironment casted_obj = (CommandEnvironment) obj;
-                boolean p0 = casted_obj.getCommandPath().equals(command);
-                boolean p1 = casted_obj.getCommandOptions().equals(options);
-                return p0 && p1;
-            }
-            return false;
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((command == null) ? 0 : command.hashCode());
+            result = prime * result + Arrays.hashCode(options);
+            return result;
         }
 
-        /**
-         * @com.intel.drl.spec_ref
-         */
-        public int hashCode() {
-            int hc = 0;
-            if (command != null) {
-                hc = hc ^ command.hashCode();
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
             }
-            if (options != null) {
-                hc = hc ^ options.hashCode();
+            if (obj == null) {
+                return false;
             }
-            return hc;
+            if (!(obj instanceof CommandEnvironment)) {
+                return false;
+            }
+            final CommandEnvironment that = (CommandEnvironment) obj;
+            if (!(command == null ? that.command == null : command.equals(that.command))) {
+                return false;
+            }
+            return Arrays.equals(options, that.options);
         }
     }
 }
