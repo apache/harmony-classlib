b726484e7b2802dfc08b1993dec6deb2d5ff7ff4
Alexey Petrenko <apetrenko@apache.org>
2006-11-02 15:20:33 +0000

    Patch from HARMONY-1509 applied OceanTheme added
    git-svn-id: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/classlib/trunk@470383 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/swing/src/main/java/common/javax/swing/plaf/metal/OceanTheme.java
@@ -0,0 +1,85 @@
+package javax.swing.plaf.metal;
+
+import javax.swing.UIDefaults;
+import javax.swing.plaf.ColorUIResource;
+
+public class OceanTheme extends DefaultMetalTheme {
+
+    @Override
+    public String getName() {
+        return "Ocean";
+    }
+
+    @Override
+    protected ColorUIResource getPrimary1() {
+        // TODO implement
+        return super.getPrimary1();
+    }
+
+    @Override
+    protected ColorUIResource getPrimary2() {
+        // TODO implement
+        return super.getPrimary2();
+    }
+
+    @Override
+    protected ColorUIResource getPrimary3() {
+        // TODO implement
+        return super.getPrimary3();
+    }
+
+    @Override
+    protected ColorUIResource getSecondary1() {
+        // TODO implement
+        return super.getSecondary1();
+    }
+
+    @Override
+    protected ColorUIResource getSecondary2() {
+        // TODO implement
+        return super.getSecondary2();
+    }
+
+    @Override
+    protected ColorUIResource getSecondary3() {
+        // TODO implement
+        return super.getSecondary3();
+    }
+
+    @Override
+    public void addCustomEntriesToTable(UIDefaults uiDefs) {
+        // TODO implement
+        super.addCustomEntriesToTable(uiDefs);
+    }
+
+    @Override
+    protected ColorUIResource getBlack() {
+        // TODO implement
+        return super.getBlack();
+    }
+
+    @Override
+    public ColorUIResource getControlTextColor() {
+        // TODO implement
+        return super.getControlTextColor();
+    }
+
+    @Override
+    public ColorUIResource getDesktopColor() {
+        // TODO implement
+        return super.getDesktopColor();
+    }
+
+    @Override
+    public ColorUIResource getInactiveControlTextColor() {
+        // TODO implement
+        return super.getInactiveControlTextColor();
+    }
+
+    @Override
+    public ColorUIResource getMenuDisabledForeground() {
+        // TODO implement
+        return super.getMenuDisabledForeground();
+    }
+
+}
