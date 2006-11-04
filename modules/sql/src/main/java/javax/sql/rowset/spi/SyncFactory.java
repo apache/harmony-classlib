9bcd3ea9940aad98160bf2e49637a66418f6a3e2
Nathan Beyer <ndbeyer@apache.org>
2006-11-04 06:57:27 +0000

    Stub out javax.sql.rowset and javax.sql.rowset.spi
    git-svn-id: https://svn.apache.org/repos/asf/incubator/harmony/enhanced/classlib/trunk@471131 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/sql/src/main/java/javax/sql/rowset/spi/SyncFactory.java
@@ -0,0 +1,68 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package javax.sql.rowset.spi;
+
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.Context;
+
+public class SyncFactory {
+    public static String ROWSET_SYNC_PROVIDER = "rowset.provider.classname";
+
+    public static String ROWSET_SYNC_VENDOR = "rowset.provider.vendor";
+
+    public static String ROWSET_SYNC_PROVIDER_VERSION = "rowset.provider.version";
+
+    public static void registerProvider(String providerID) throws SyncFactoryException {
+        throw new UnsupportedOperationException();
+    }
+
+    public static SyncFactory getSyncFactory() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void unregisterProvider(String providerID) throws SyncFactoryException {
+        throw new UnsupportedOperationException();
+    }
+
+    public static SyncProvider getInstance(String providerID) throws SyncFactoryException {
+        throw new UnsupportedOperationException();
+    }
+
+    public static Enumeration<SyncProvider> getRegisteredProviders()
+            throws SyncFactoryException {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void setLogger(Logger logger) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void setLogger(Logger logger, Level level) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static Logger getLogger() throws SyncFactoryException {
+        throw new UnsupportedOperationException();
+    }
+
+    public static void setJNDIContext(Context ctx) throws SyncFactoryException {
+        throw new UnsupportedOperationException();
+    }
+}
