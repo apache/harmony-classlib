c93547d54f29952b9dffcef27d2120df8a881329
Mark Hindess <hindessm@apache.org>
2007-01-16 17:10:21 +0000

    Applying patches from "[#HARMONY-1767] [classlib][swing]javax.swing.text.html.ComponentView(Element) throws unexpected NullPointerException on user's implementation".
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@496771 13f79535-47bb-0310-9956-ffa450edef68

--- modules/swing/src/test/api/java/common/javax/swing/text/ComponentViewTest.java
+++ modules/swing/src/test/api/java/common/javax/swing/text/ComponentViewTest.java
@@ -33,6 +33,10 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextPane;
 import javax.swing.SwingTestCase;
+import javax.swing.text.Element;
+import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.ListView;
+
 
 public class ComponentViewTest extends SwingTestCase {
     StyledDocument document;
@@ -646,6 +650,47 @@ public class ComponentViewTest extends SwingTestCase {
             assertNotSame(textPane, view.getComponent().getParent());
             assertFalse(textPane.equals(view.getComponent().getParent()));
             assertSame(textPane, view.getComponent().getParent().getParent());
+        }
+    }
+
+    public void testSetParent_View() {
+        // Regression test for HARMONY-1767 
+        HTMLDocument doc = new HTMLDocument();
+        Element e = doc.getDefaultRootElement();
+        ComponentView obj = new ComponentView(new TestElement());
+        obj.setParent(new ListView(e));        
+    }  
+
+    private class TestElement implements Element {
+        public boolean isLeaf() {
+            return false;
+        }
+        public Element getElement(int index) {
+            return null;
+        }
+        public int getElementCount() {
+            return 0;
+        }
+        public int getElementIndex(int offset) {
+            return 0;
+        }
+        public int getEndOffset() {
+            return 0;
+        }       
+        public int getStartOffset() {
+            return 0;
+        }       
+        public AttributeSet getAttributes() {
+            return null;
+        }
+        public String getName() {
+            return "AA";
+        }
+        public Element getParentElement() {
+            return null;
+        }
+        public Document getDocument() {
+            return null;
         }
     }
 
