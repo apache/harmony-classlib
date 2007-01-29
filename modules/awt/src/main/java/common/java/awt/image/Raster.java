700b6af74c743d75fdf0f898fb3553c00fecb362
Alexei Zakharov <ayza@apache.org>
2007-01-29 13:21:15 +0000

    Reverting invalid patch for HARMONY-2885 ([classlib][awt] Compatibility: java.awt.image.Raster.createRaster() doesn't throw RasterFormatException if DataBuffer type is incorrect)
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@501036 13f79535-47bb-0310-9956-ffa450edef68

--- modules/awt/src/main/java/common/java/awt/image/Raster.java
+++ modules/awt/src/main/java/common/java/awt/image/Raster.java
@@ -534,11 +534,6 @@ public class Raster {
             throw new NullPointerException(Messages.getString("awt.27F")); //$NON-NLS-1$
         }
 
-        if (sm.dataType != db.dataType) {
-            // awt.293=SampleModel and DataBuffer have different data types
-            throw new RasterFormatException(Messages.getString("awt.293")); //$NON-NLS-1$
-        }
-        
         if (location == null) {
             location = new Point(0, 0);
         }
