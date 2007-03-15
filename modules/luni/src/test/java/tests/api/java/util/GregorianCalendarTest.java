5566e8752a7b6db4c350dd5284d2fddf50c05b95
Nathan Beyer <ndbeyer@apache.org>
2007-03-15 03:39:47 +0000

    Apply second patch for HARMONY-2959: [classlib][luni]GregorianCalendar.get(DAY_OF_YEAR) returns incorrect value sometimes
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@518459 13f79535-47bb-0310-9956-ffa450edef68

--- modules/luni/src/test/java/tests/api/java/util/GregorianCalendarTest.java
+++ modules/luni/src/test/java/tests/api/java/util/GregorianCalendarTest.java
@@ -659,14 +659,14 @@ public class GregorianCalendarTest extends junit.framework.TestCase {
 	public void test_getI() { 
         // Regression test for HARMONY-2959
         Date date = new Date(Date.parse("Jan 15 00:00:01 GMT 2000")); 
-        GregorianCalendar gc = new GregorianCalendar(); 
+        GregorianCalendar gc = new GregorianCalendar(TimeZone.getTimeZone("GMT")); 
         gc.setGregorianChange(date); 
         gc.setTimeInMillis(Date.parse("Dec 24 00:00:01 GMT 2000")); 
         assertEquals(346, gc.get(Calendar.DAY_OF_YEAR)); 
         
         // Regression test for HARMONY-3003
         date = new Date(Date.parse("Feb 28 00:00:01 GMT 2000"));
-        gc = new GregorianCalendar();
+        gc = new GregorianCalendar(TimeZone.getTimeZone("GMT"));
         gc.setGregorianChange(date);
         gc.setTimeInMillis(Date.parse("Dec 1 00:00:01 GMT 2000"));
         assertEquals(1, gc.get(Calendar.DAY_OF_MONTH));
