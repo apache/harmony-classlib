0b4813f9a1f102fa7402e95ddf3b4d725f3c734d
Alexey Petrenko <apetrenko@apache.org>
2007-03-21 08:36:01 +0000

    Patch for HARMONY-3435 "[classlib][luni]move out src/test/java/tests/api/java/net/SocketImplTest.java"
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@520801 13f79535-47bb-0310-9956-ffa450edef68

--- modules/luni/src/test/java/tests/api/java/net/ExcludedProxyTest.java
+++ modules/luni/src/test/java/tests/api/java/net/ExcludedProxyTest.java
@@ -20,6 +20,7 @@ package tests.api.java.net;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.Socket;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -103,6 +104,46 @@ public class ExcludedProxyTest extends TestCase {
                 response += new String(b4, 0, count);
             assertTrue("Response to POST method invalid", response
                     .equals(posted4));
+        } finally {
+            System.setProperties(null);
+        }
+    }
+    
+    /**
+     * @tests java.net.SocketImpl#SocketImpl()
+     */
+    public void test_Constructor() {
+        try {
+            try {
+                System.setProperty("socksProxyHost",
+                        Support_Configuration.SocksServerTestHost);
+                System.setProperty("socksProxyPort", String
+                        .valueOf(Support_Configuration.SocksServerTestPort));
+                Socket s = new Socket(Support_Configuration.HomeAddress, 80);
+                OutputStream os = s.getOutputStream();
+                os.write("GET / HTTP/1.0\r\n\r\n".getBytes());
+                s.getInputStream();
+            } catch (IOException e) {
+                fail("Could not open socket: "
+                        + Support_Configuration.HomeAddress + " " + e);
+            }
+            boolean exception = false;
+            try {
+                System.setProperty("socksProxyHost",
+                        Support_Configuration.SocksServerTestHost);
+                System
+                        .setProperty(
+                                "socksProxyPort",
+                                String
+                                        .valueOf(Support_Configuration.SocksServerTestPort + 1));
+                Socket s = new Socket(Support_Configuration.HomeAddress, 80);
+                OutputStream os = s.getOutputStream();
+                os.write("GET / HTTP/1.0\r\n\r\n".getBytes());
+                s.getInputStream();
+            } catch (IOException e) {
+                exception = true;
+            }
+            assertTrue("Exception should have been thrown", exception);
         } finally {
             System.setProperties(null);
         }
--- modules/luni/src/test/java/tests/api/java/net/SocketImplTest.java
+++ modules/luni/src/test/java/tests/api/java/net/SocketImplTest.java
@@ -22,54 +22,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
-import java.net.Socket;
 import java.net.SocketAddress;
 import java.net.SocketException;
 import java.net.SocketImpl;
 
-import tests.support.Support_Configuration;
-
 public class SocketImplTest extends junit.framework.TestCase {
 
-	/**
-	 * @tests java.net.SocketImpl#SocketImpl()
-	 */
-	public void test_Constructor() {
-		try {
-			try {
-				System.setProperty("socksProxyHost",
-						Support_Configuration.SocksServerTestHost);
-				System.setProperty("socksProxyPort", String
-						.valueOf(Support_Configuration.SocksServerTestPort));
-				Socket s = new Socket(Support_Configuration.HomeAddress, 80);
-				OutputStream os = s.getOutputStream();
-				os.write("GET / HTTP/1.0\r\n\r\n".getBytes());
-				s.getInputStream();
-			} catch (IOException e) {
-				fail("Could not open socket: "
-						+ Support_Configuration.HomeAddress + " " + e);
-			}
-			boolean exception = false;
-			try {
-				System.setProperty("socksProxyHost",
-						Support_Configuration.SocksServerTestHost);
-				System
-						.setProperty(
-								"socksProxyPort",
-								String
-										.valueOf(Support_Configuration.SocksServerTestPort + 1));
-				Socket s = new Socket(Support_Configuration.HomeAddress, 80);
-				OutputStream os = s.getOutputStream();
-				os.write("GET / HTTP/1.0\r\n\r\n".getBytes());
-				s.getInputStream();
-			} catch (IOException e) {
-				exception = true;
-			}
-			assertTrue("Exception should have been thrown", exception);
-		} finally {
-			System.setProperties(null);
-		}
-	}
 	
     public void test_Constructor_fd() throws Exception {
         // regression test for Harmony-1117
