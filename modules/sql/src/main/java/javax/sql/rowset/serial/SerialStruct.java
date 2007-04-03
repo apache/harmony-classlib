f1e1a7a3a876d0a16587c164113d17db47a4dcb2
Stepan Mishura <smishura@apache.org>
2007-04-03 04:02:46 +0000

    Apply patch for HARMONY-3531: ([classlib][sql] implementation and test case stub for javax.sql.rowset.serial.SerialStruct)
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@525019 13f79535-47bb-0310-9956-ffa450edef68

---
+++ modules/sql/src/main/java/javax/sql/rowset/serial/SerialStruct.java
@@ -0,0 +1,64 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package javax.sql.rowset.serial;
+
+import java.io.Serializable;
+import java.sql.SQLData;
+import java.sql.SQLException;
+import java.sql.Struct;
+import java.util.Map;
+
+import org.apache.harmony.luni.util.NotImplementedException;
+
+public class SerialStruct implements Struct, Serializable, Cloneable {
+    // required by serialized form
+    @SuppressWarnings("unused")
+    private static final long serialVersionUID = -8322445504027483372L;
+
+    // required by serialized form
+    private String SQLTypeName;
+
+    // required by serialized form
+    private Object[] attribs;
+
+    public SerialStruct(SQLData in, Map<String, Class<?>> map)
+            throws SerialException, NotImplementedException {
+        throw new NotImplementedException();
+    }
+
+    public SerialStruct(Struct in, Map<String, Class<?>> map)
+            throws SerialException, NotImplementedException {
+        throw new NotImplementedException();
+    }
+
+    public Object[] getAttributes() throws SerialException,
+            NotImplementedException {
+        throw new NotImplementedException();
+
+    }
+
+    public Object[] getAttributes(Map<String, Class<?>> map)
+            throws SerialException, NotImplementedException {
+        throw new NotImplementedException();
+    }
+
+    public String getSQLTypeName() throws SQLException, NotImplementedException {
+        throw new NotImplementedException();
+    }
+
+}
