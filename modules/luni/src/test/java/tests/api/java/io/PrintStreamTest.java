816149c9ba148c4fcb05dbf7ab48c7873d9c8fad
Yang Pu <pyang@apache.org>
2007-04-11 11:59:53 +0000

    Apply patch for HARMONY-3608([classlib][luni] Add test cases for java.io.PrintStream)
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@527462 13f79535-47bb-0310-9956-ffa450edef68

--- modules/luni/src/test/java/tests/api/java/io/PrintStreamTest.java
+++ modules/luni/src/test/java/tests/api/java/io/PrintStreamTest.java
@@ -20,9 +20,12 @@ package tests.api.java.io;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 import java.util.Locale;
 
 public class PrintStreamTest extends junit.framework.TestCase {
@@ -30,8 +33,50 @@ public class PrintStreamTest extends junit.framework.TestCase {
     ByteArrayOutputStream bos = new ByteArrayOutputStream();
 
     byte[] ibuf = new byte[4096];
+    
+    private File testFile = null;
+    
+    private String testFilePath = null;
 
     public String fileString = "Test_All_Tests\nTest_java_io_BufferedInputStream\nTest_java_io_BufferedOutputStream\nTest_java_io_ByteArrayInputStream\nTest_java_io_ByteArrayOutputStream\nTest_java_io_DataInputStream\nTest_java_io_File\nTest_java_io_FileDescriptor\nTest_java_io_FileInputStream\nTest_java_io_FileNotFoundException\nTest_java_io_FileOutputStream\nTest_java_io_FilterInputStream\nTest_java_io_FilterOutputStream\nTest_java_io_InputStream\nTest_java_io_IOException\nTest_java_io_OutputStream\nTest_PrintStream\nTest_java_io_RandomAccessFile\nTest_java_io_SyncFailedException\nTest_java_lang_AbstractMethodError\nTest_java_lang_ArithmeticException\nTest_java_lang_ArrayIndexOutOfBoundsException\nTest_java_lang_ArrayStoreException\nTest_java_lang_Boolean\nTest_java_lang_Byte\nTest_java_lang_Character\nTest_java_lang_Class\nTest_java_lang_ClassCastException\nTest_java_lang_ClassCircularityError\nTest_java_lang_ClassFormatError\nTest_java_lang_ClassLoader\nTest_java_lang_ClassNotFoundException\nTest_java_lang_CloneNotSupportedException\nTest_java_lang_Double\nTest_java_lang_Error\nTest_java_lang_Exception\nTest_java_lang_ExceptionInInitializerError\nTest_java_lang_Float\nTest_java_lang_IllegalAccessError\nTest_java_lang_IllegalAccessException\nTest_java_lang_IllegalArgumentException\nTest_java_lang_IllegalMonitorStateException\nTest_java_lang_IllegalThreadStateException\nTest_java_lang_IncompatibleClassChangeError\nTest_java_lang_IndexOutOfBoundsException\nTest_java_lang_InstantiationError\nTest_java_lang_InstantiationException\nTest_java_lang_Integer\nTest_java_lang_InternalError\nTest_java_lang_InterruptedException\nTest_java_lang_LinkageError\nTest_java_lang_Long\nTest_java_lang_Math\nTest_java_lang_NegativeArraySizeException\nTest_java_lang_NoClassDefFoundError\nTest_java_lang_NoSuchFieldError\nTest_java_lang_NoSuchMethodError\nTest_java_lang_NullPointerException\nTest_java_lang_Number\nTest_java_lang_NumberFormatException\nTest_java_lang_Object\nTest_java_lang_OutOfMemoryError\nTest_java_lang_RuntimeException\nTest_java_lang_SecurityManager\nTest_java_lang_Short\nTest_java_lang_StackOverflowError\nTest_java_lang_String\nTest_java_lang_StringBuffer\nTest_java_lang_StringIndexOutOfBoundsException\nTest_java_lang_System\nTest_java_lang_Thread\nTest_java_lang_ThreadDeath\nTest_java_lang_ThreadGroup\nTest_java_lang_Throwable\nTest_java_lang_UnknownError\nTest_java_lang_UnsatisfiedLinkError\nTest_java_lang_VerifyError\nTest_java_lang_VirtualMachineError\nTest_java_lang_vm_Image\nTest_java_lang_vm_MemorySegment\nTest_java_lang_vm_ROMStoreException\nTest_java_lang_vm_VM\nTest_java_lang_Void\nTest_java_net_BindException\nTest_java_net_ConnectException\nTest_java_net_DatagramPacket\nTest_java_net_DatagramSocket\nTest_java_net_DatagramSocketImpl\nTest_java_net_InetAddress\nTest_java_net_NoRouteToHostException\nTest_java_net_PlainDatagramSocketImpl\nTest_java_net_PlainSocketImpl\nTest_java_net_Socket\nTest_java_net_SocketException\nTest_java_net_SocketImpl\nTest_java_net_SocketInputStream\nTest_java_net_SocketOutputStream\nTest_java_net_UnknownHostException\nTest_java_util_ArrayEnumerator\nTest_java_util_Date\nTest_java_util_EventObject\nTest_java_util_HashEnumerator\nTest_java_util_Hashtable\nTest_java_util_Properties\nTest_java_util_ResourceBundle\nTest_java_util_tm\nTest_java_util_Vector\n";
+    
+    private static class MockPrintStream extends PrintStream {
+
+		public MockPrintStream(String fileName) throws FileNotFoundException {
+			super(fileName);
+		}
+		
+		public MockPrintStream(String fileName, String csn) throws FileNotFoundException, UnsupportedEncodingException {
+			super(fileName, csn);
+		}
+		
+		public MockPrintStream(OutputStream os) {
+			super(os);
+		}
+		
+		@Override
+		public void setError() {
+			super.setError();
+		}
+    }
+    
+    /**
+     * @tests {@link java.io.PrintStream#PrintStream(String)}
+     */
+    public void test_Constructor_Ljava_lang_String() throws IOException {
+    	MockPrintStream os = new MockPrintStream(testFilePath);
+    	assertNotNull(os);
+    	os.close();
+	}
+    
+    /**
+     * @tests {@link java.io.PrintStream#PrintStream(String, String)}
+     */
+    public void test_Constructor_Ljava_lang_String_Ljava_lang_String() throws Exception {
+    	MockPrintStream os = new MockPrintStream(testFilePath, "utf-8");
+    	assertNotNull(os);
+    	os.close();
+    }
 
     /**
      * @tests java.io.PrintStream#PrintStream(java.io.OutputStream)
@@ -555,4 +600,21 @@ public class PrintStreamTest extends junit.framework.TestCase {
         assertEquals("Wrote incorrect string", "Hello World",
                 new String(rbytes));
     }
+
+	@Override
+	protected void setUp() throws Exception {		
+		super.setUp();
+		testFile = File.createTempFile("test", null);
+		testFilePath = testFile.getAbsolutePath();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		testFile.delete();
+		testFile = null;
+		testFilePath = null;
+		super.tearDown();
+	}
+    
+    
 }
