29f22b73914de591b9c8a66a47184812d1a3e96c
Alexey Petrenko <apetrenko@apache.org>
2007-05-28 13:37:27 +0000

    Additional patch for HARMONY-3904 "[classlib][swing] javax.swing.plaf.multi package is unimplemented"
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@542226 13f79535-47bb-0310-9956-ffa450edef68

--- modules/swing/src/test/api/java.injected/javax/swing/plaf/multi/SyserrButtonUI.java
+++ modules/swing/src/test/api/java.injected/javax/swing/plaf/multi/SyserrButtonUI.java
@@ -6,15 +6,19 @@ import javax.swing.JComponent;
 import javax.swing.plaf.ButtonUI;
 import javax.swing.plaf.ComponentUI;
 
+/**
+ * Auxiliary ui for multi package testing. Console mesages has been removed to
+ * tidy unit tests console output
+ */
 public class SyserrButtonUI extends ButtonUI {
 
-	public static ComponentUI createUI(JComponent a) {
-		System.err.println("create");
-		return new SyserrButtonUI();
-	}
+    public static ComponentUI createUI(JComponent a) {
+        // System.err.println("create");
+        return new SyserrButtonUI();
+    }
 
-	@Override
-	public void paint(Graphics a, JComponent b) {
-		System.err.println("paint");
-	}
+    @Override
+    public void paint(Graphics a, JComponent b) {
+        // System.err.println("paint");
+    }
 }
