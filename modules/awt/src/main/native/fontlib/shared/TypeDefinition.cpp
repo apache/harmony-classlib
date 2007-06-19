49bc35d899a97b5585ecff316ff94ad09d352861
Alexey Petrenko <apetrenko@apache.org>
2007-06-19 08:06:41 +0000

    Fontlib crash on em64t fix
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@548644 13f79535-47bb-0310-9956-ffa450edef68

--- modules/awt/src/main/native/fontlib/shared/TypeDefinition.cpp
+++ modules/awt/src/main/native/fontlib/shared/TypeDefinition.cpp
@@ -2,6 +2,9 @@
 
 fint fwcslen(fwchar_t* str)
 {
+    if (!str)
+        return 0;
+        
 	fint counter=0;
 	fwchar_t* tmpstr = str;
 	while(*tmpstr != 0)
@@ -14,6 +17,9 @@ fint fwcslen(fwchar_t* str)
 
 fint fwcscmp(fwchar_t* str1, fwchar_t* str2)
 {
+    if (str1 == 0 || str2 == 0)
+        return str1 - str2;
+        
 	fwchar_t* tmpstr1 = str1; 
 	fwchar_t* tmpstr2 = str2;
 	while(*tmpstr1 != 0 || *tmpstr2 !=0)
