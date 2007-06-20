8a57166a9805f8f7346193053bc148353a77ff2b
Wu Yue Hao <tonywu@apache.org>
2007-06-20 07:00:25 +0000

    Apply patch HARMONY-4240 ([classlib][sql][java6] new interface java.sql.Wrapper and javax.sql.CommonDataSource)
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/branches/java6@548961 13f79535-47bb-0310-9956-ffa450edef68

--- modules/sql/src/main/java/javax/sql/DataSource.java
+++ modules/sql/src/main/java/javax/sql/DataSource.java
@@ -17,9 +17,9 @@
 
 package javax.sql;
 
-import java.sql.SQLException;
 import java.sql.Connection;
-import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.sql.Wrapper;
 
 /**
  * An interface for the creation of Connection objects which represent a
@@ -50,7 +50,7 @@ import java.io.PrintWriter;
  * loaded via a JNDI lookup process. A driver loaded in this way does not
  * register itself with the <code>DriverManager</code>.
  */
-public interface DataSource {
+public interface DataSource extends CommonDataSource, Wrapper{
 
     /**
      * Creates a connection to the database represented by this DataSource.
@@ -75,64 +75,5 @@ public interface DataSource {
      *             if there is a problem accessing the database.
      */
     public Connection getConnection(String theUsername, String thePassword)
-            throws SQLException;
-
-    /**
-     * Gets the Login Timeout value for this DataSource. The Login Timeout is
-     * the maximum time in seconds that the DataSource will wait when opening a
-     * connection to a database. A Timeout value of 0 implies either the system
-     * default timeout value (if there is one) or that there is no timeout. The
-     * default value for the Login Timeout is 0.
-     * 
-     * @return the Login Timeout value in seconds.
-     * @throws SQLException
-     *             if there is a problem accessing the database.
-     */
-    public int getLoginTimeout() throws SQLException;
-
-    /**
-     * Gets the Log Writer for this DataSource.
-     * <p>
-     * The Log Writer is a stream to which all log and trace messages are sent
-     * from this DataSource. The Log Writer can be null, in which case, log and
-     * trace capture is disabled. The default value for the Log Writer when an
-     * DataSource is created is null. Note that the Log Writer for an DataSource
-     * is not the same as the Log Writer used by a <code>DriverManager</code>.
-     * 
-     * @return a PrintWriter which is the Log Writer for this DataSource. Can be
-     *         null, in which case log writing is disabled for this DataSource.
-     * @throws SQLException
-     *             if there is a problem accessing the database.
-     */
-    public PrintWriter getLogWriter() throws SQLException;
-
-    /**
-     * Sets the Login Timeout value for this DataSource. The Login Timeout is
-     * the maximum time in seconds that the DataSource will wait when opening a
-     * connection to a database. A Timeout value of 0 implies either the system
-     * default timeout value (if there is one) or that there is no timeout. The
-     * default value for the Login Timeout is 0.
-     * 
-     * @param theTimeout
-     *            the new Login Timeout value in seconds.
-     * @throws SQLException
-     *             if there is a problem accessing the database.
-     */
-    public void setLoginTimeout(int theTimeout) throws SQLException;
-
-    /**
-     * Sets the Log Writer for this DataSource.
-     * <p>
-     * The Log Writer is a stream to which all log and trace messages are sent
-     * from this DataSource. The Log Writer can be null, in which case, log and
-     * trace capture is disabled. The default value for the Log Writer when an
-     * DataSource is created is null. Note that the Log Writer for an DataSource
-     * is not the same as the Log Writer used by a <code>DriverManager</code>.
-     * 
-     * @param theWriter
-     *            a PrintWriter to use as the Log Writer for this DataSource.
-     * @throws SQLException
-     *             if there is a problem accessing the database.
-     */
-    public void setLogWriter(PrintWriter theWriter) throws SQLException;
+            throws SQLException;    
 }
