553b1c8e328fc31662ae0342fe0e8f9a44636645
Wu Yue Hao <tonywu@apache.org>
2007-07-18 02:29:50 +0000

    Write javadoc for java.beans.Beans, no functional change
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@557119 13f79535-47bb-0310-9956-ffa450edef68

--- modules/beans/src/main/java/java/beans/Beans.java
+++ modules/beans/src/main/java/java/beans/Beans.java
@@ -36,26 +36,120 @@ import java.util.Vector;
 
 import org.apache.harmony.beans.internal.nls.Messages;
 
+/**
+ * This class <code>Beans</code> provides some methods for manipulting bean
+ * controls.
+ * 
+ */
+
 public class Beans {
 
     private static boolean designTime = false;
 
     private static boolean guiAvailable = true;
 
+    /**
+     * Constructs a Beans instance.
+     */
     public Beans() {
     }
 
+    /**
+     * Obtains an instance of a JavaBean specified the bean name using the
+     * specified class loader.
+     * <p>
+     * If the specified class loader is null, the system class loader is used.
+     * </p>
+     * 
+     * @param loader
+     *            the specified class loader. It can be null.
+     * @param name
+     *            the name of the JavaBean
+     * @return an isntance of the bean.
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
     public static Object instantiate(ClassLoader loader, String name)
             throws IOException, ClassNotFoundException {
         return instantiate(loader, name, null, null);
     }
 
+    /**
+     * Obtains an instance of a JavaBean specified the bean name using the
+     * specified class loader, and adds the instance into the specified bean
+     * context.
+     * 
+     * <p>
+     * If the specified class loader is null, the system class loader is used.
+     * </p>
+     * 
+     * @param loader
+     *            the specified class loader. It can be null.
+     * @param name
+     *            the name of the JavaBean
+     * @param context
+     *            the beancontext in which the bean instance will be added.
+     * @return an instance of the specified JavaBean.
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
     public static Object instantiate(ClassLoader cls, String beanName,
 			BeanContext beanContext) throws IOException, ClassNotFoundException {
 		return instantiate(cls, beanName, beanContext, null);
 
 	}
 
+    /**
+     * Obtains an instance of a JavaBean specified by the bean name using the
+     * specified class loader, and adds the instance into the specified bean
+     * context.
+     * <p>
+     * The parameter name must be a qualified name relative to the specified
+     * class loader. For example, "java.awt.Button" and "x.y.z".
+     * </p>
+     * <p>
+     * If the specified class loader is null, the system class loader is used.
+     * </p>
+     * <p>
+     * Firstly, The class <code>Beans</code> regards the bean name as a
+     * serialized object name. The class <code>Beans</code> convert bean name
+     * into pathname, append a suffix ".ser" to the pathname. then try to load
+     * the resource using the specified class loader. If <code>Beans</code>
+     * fails to load the resource, <code>Beans</code> will regard the
+     * <code>name</code> as a class name and construct a new instance of the
+     * bean class.
+     * </p>
+     * <p>
+     * For example, if the name is specified as "x.y.z", The class
+     * <code>Beans</code> will try to load the serialized object from the
+     * resource "x/y/z.ser"; If <code>Beans</code> fails to load the resource
+     * "x/y/z.ser", it will create a new instance of "x.y.z".
+     * </p>
+     * <p>
+     * If the bean is an instance of java.applet.Applet, <code>Beans</code>
+     * will do some special initialization for this applet bean. First,
+     * <code>Beans</code> will set the default AppletStub and AppletContext
+     * for the applet bean (If the specified <code>AppletInitializer</code> is
+     * not null, <code>Beans</code> will call the
+     * <code>AppletInitializer.initialize</code> to set the default AppletStub
+     * and AppletContext for the applet bean). Second, <code>Beans</code> will
+     * call the <code>init</code> method of the applet. (If the applet bean is
+     * loaded as a serialized object, the <code>init</code> method will not be
+     * called.)
+     * </p>
+     * 
+     * @param loader
+     *            the specified class loader. It can be null.
+     * @param name
+     *            the name of the JavaBean
+     * @param context
+     *            the beancontext in which the bean instance will be added.
+     * @param initializer
+     *            the AppletInitializer for applet bean instance.
+     * @return Obtains an instance of the JavaBean.
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
     @SuppressWarnings("unchecked")
 	public static Object instantiate(ClassLoader cls, String beanName,
 			BeanContext context, AppletInitializer initializer)
@@ -110,10 +204,35 @@ public class Beans {
 		return result;
 	}
 
+    /**
+     * Obtain an alternative type view of the given bean. The view type is
+     * specified by the parameter <code>type</code>.
+     * <p>
+     * If the type view cannot be obtained, the original bean object is
+     * returned.
+     * </p>
+     * 
+     * @param bean
+     *            the original bean object.
+     * @param type
+     *            the specified view type.
+     * @return a type view of the given bean.
+     */
     public static Object getInstanceOf(Object bean, Class<?> targetType) {
         return bean;
     }
 
+    /**
+     * Determine if the the specified bean object can be viewed as the specified
+     * type.
+     * 
+     * @param bean
+     *            the specified bean object.
+     * @param type
+     *            the specifed view type.
+     * @return true if the specified bean object can be viewed as the specified
+     *         type; otherwise, return false;
+     */
     public static boolean isInstanceOf(Object bean, Class<?> targetType) {
         if (bean == null) {
             throw new NullPointerException(Messages.getString("beans.1D")); //$NON-NLS-1$
@@ -122,12 +241,31 @@ public class Beans {
         return targetType == null ? false : targetType.isInstance(bean);
     }
 
+    /**
+     * Set whether or not a GUI is available in the bean's current environment.
+     * 
+     * @param value
+     *            should be <code>true</code> to signify that a GUI is
+     *            available, <code>false</code> otherwise.
+     * @throws SecurityException
+     *             if the caller does not have the required permission to access
+     *             or modify system properties.
+     */
     public static synchronized void setGuiAvailable(boolean isGuiAvailable)
             throws SecurityException {
         checkPropertiesAccess();
         guiAvailable = isGuiAvailable;
     }
-
+    
+    /**
+     * Used to indicate whether of not it's in an application construction
+     * environment.
+     * 
+     * @param value
+     *            true to indicate that it's in application construction
+     *            environment.
+     * @throws SecurityException
+     */
     public static void setDesignTime(boolean isDesignTime)
             throws SecurityException {
         checkPropertiesAccess();
@@ -136,10 +274,22 @@ public class Beans {
         }
     }
 
+    /**
+     * Returns a boolean indication of whether or not a GUI is available for
+     * beans.
+     * 
+     * @return <code>true</code> if a GUI is available, otherwise
+     *         <code>false</code>.
+     */
     public static synchronized boolean isGuiAvailable() {
         return guiAvailable;
     }
 
+    /**
+     * Determine if it's in design-mode.
+     * 
+     * @return true if it's in an application construction environment.
+     */
     public static synchronized boolean isDesignTime() {
         return designTime;
     }
