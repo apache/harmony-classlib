07ba5a85337882909ba57901b6d38f685a344bef
Li Yi Lei <leoli@apache.org>
2007-08-20 07:00:23 +0000

    Apply patch for HARMONY-3148( [Classlib][nio] alloc many DirectByteBuffers may cause memory-out-error).
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@567561 13f79535-47bb-0310-9956-ffa450edef68

--- modules/luni/src/main/native/luni/windows/OSMemory.c
+++ modules/luni/src/main/native/luni/windows/OSMemory.c
@@ -24,7 +24,7 @@
 #include "IMemorySystem.h"
 #include "exceptions.h"
 
-JNIEXPORT jlong JNICALL Java_org_apache_harmony_luni_platform_OSMemory_malloc
+JNIEXPORT jlong JNICALL Java_org_apache_harmony_luni_platform_OSMemory_mallocNative
   (JNIEnv * env, jobject thiz, jlong size)
 {
   PORT_ACCESS_FROM_ENV (env);
