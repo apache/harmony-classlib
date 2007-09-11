apache/harmony-classlib

commit ce56decae7709a0d14415a31a8766471315c5260

Applying corresponding changes to those recently applied to OSNetworkSystemLinux.c.
git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@574370 13f79535-47bb-0310-9956-ffa450edef68

modules/luni/src/main/native/luni/windows/OSNetworkSystemWin32.c
@@ -214,6 +214,7 @@ selectRead (JNIEnv * env,hysocket_t hysocketP, I_32 uSecTime, BOOLEAN accept){
  */
 JNIEXPORT jint JNICALL Java_org_apache_harmony_luni_platform_OSNetworkSystem_isReachableByICMPImpl
   (JNIEnv * env, jobject clz, jobject address,jobject localaddr, jint ttl, jint timeout){
+  PORT_ACCESS_FROM_ENV (env);
   struct sockaddr_in dest,source,local;
   struct ICMPHeader* send_buf = 0;
   struct IPHeader* recv_buf = 0;
@@ -262,9 +263,12 @@ JNIEXPORT jint JNICALL Java_org_apache_harmony_luni_platform_OSNetworkSystem_isR
   }
   
   // set basic send and recv buf
-  send_buf = (struct ICMPHeader*)malloc(sizeof(char)*ICMP_SIZE);
-  recv_buf = (struct IPHeader*)malloc(sizeof(char)*PACKET_SIZE);
-  if (NULL == send_buf || NULL == recv_buf){
+  send_buf = (struct ICMPHeader*)hymem_allocate_memory(sizeof(char)*ICMP_SIZE);
+  if (NULL == send_buf) {
+	  goto cleanup;
+  }
+  recv_buf = (struct IPHeader*)hymem_allocate_memory(sizeof(char)*PACKET_SIZE);
+  if (NULL == recv_buf){
 	  goto cleanup;
   }
   set_icmp_packet(send_buf, ICMP_SIZE);
@@ -273,7 +277,7 @@ JNIEXPORT jint JNICALL Java_org_apache_harmony_luni_platform_OSNetworkSystem_isR
             (struct sockaddr*)&dest, sizeof(dest))){
 	  goto cleanup;
   }
-  fdset_read = (fd_set *)malloc(sizeof (struct fd_set));
+  fdset_read = (fd_set *)hymem_allocate_memory(sizeof (struct fd_set));
   if (NULL == fdset_read){
   	  goto cleanup;
   }
@@ -304,9 +308,9 @@ JNIEXPORT jint JNICALL Java_org_apache_harmony_luni_platform_OSNetworkSystem_isR
   }
   ret = REACHABLE;
 cleanup:
-  free(fdset_read);
-  free(send_buf);
-  free(recv_buf);
+  hymem_allocate_memory(fdset_read);
+  hymem_allocate_memory(send_buf);
+  hymem_allocate_memory(recv_buf);
   WSACleanup();	 
   return ret;
 }
@@ -314,7 +318,7 @@ cleanup:
 // typical ip checksum
 unsigned short ip_checksum(unsigned short * buffer, int size)
 {
-	register unsigned short * buf = buffer;
+    register unsigned short * buf = buffer;
     register int bufleft = size;
     register unsigned long sum = 0;
     

commit 1a65c12481581329f47af4fd7d3a2f81258adfc8

Fix typo introduced by r574370
git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@574436 13f79535-47bb-0310-9956-ffa450edef68

modules/luni/src/main/native/luni/windows/OSNetworkSystemWin32.c
@@ -308,9 +308,9 @@ JNIEXPORT jint JNICALL Java_org_apache_harmony_luni_platform_OSNetworkSystem_isR
   }
   ret = REACHABLE;
 cleanup:
-  hymem_allocate_memory(fdset_read);
-  hymem_allocate_memory(send_buf);
-  hymem_allocate_memory(recv_buf);
+  hymem_free_memory(fdset_read);
+  hymem_free_memory(send_buf);
+  hymem_free_memory(recv_buf);
   WSACleanup();	 
   return ret;
 }