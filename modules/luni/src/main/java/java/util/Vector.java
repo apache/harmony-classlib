a072606aea7f2d3b04a27beef55ce18eb8c018d6
Jing Lv <lvjing@apache.org>
2007-11-27 11:29:38 +0000

    Methods in ArrayList and Vector should NOT call public and overridabe method size() to avoid its modification and size effect some problem.
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@598602 13f79535-47bb-0310-9956-ffa450edef68

--- modules/luni/src/main/java/java/util/Vector.java
+++ modules/luni/src/main/java/java/util/Vector.java
@@ -400,7 +400,7 @@ public class Vector<E> extends AbstractList<E> implements List<E>,
         }
         if (object instanceof List) {
             List<?> list = (List<?>) object;
-            if (list.size() != size()) {
+            if (list.size() != elementCount) {
                 return false;
             }
 
@@ -830,7 +830,7 @@ public class Vector<E> extends AbstractList<E> implements List<E>,
      */
     @Override
     protected void removeRange(int start, int end) {
-        if (start >= 0 && start <= end && end <= size()) {
+        if (start >= 0 && start <= end && end <= elementCount) {
             if (start == end) {
                 return;
             }
@@ -1022,7 +1022,7 @@ public class Vector<E> extends AbstractList<E> implements List<E>,
             return "[]"; //$NON-NLS-1$
         }
         int length = elementCount - 1;
-        StringBuffer buffer = new StringBuffer(size() * 16);
+        StringBuffer buffer = new StringBuffer(elementCount * 16);
         buffer.append('[');
         for (int i = 0; i < length; i++) {
             if (elementData[i] == this) {
