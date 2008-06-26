91ccd2bd03e3593a478c2b0223729a28c62be3ac
Mark Hindess <hindessm@apache.org>
2008-06-26 12:23:11 +0000

    Applying patches from "[#HARMONY-5880] Fixes an issue in DefaultMetalTheme that failed tests in BasicBordersTest".
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@671874 13f79535-47bb-0310-9956-ffa450edef68

--- modules/swing/src/main/java/common/javax/swing/plaf/metal/DefaultMetalTheme.java
+++ modules/swing/src/main/java/common/javax/swing/plaf/metal/DefaultMetalTheme.java
@@ -31,7 +31,7 @@ public class DefaultMetalTheme extends MetalTheme {
     private static ColorUIResource primaryColor1 = new ColorUIResource(0x20, 0x50, 0x80);
     private static ColorUIResource primaryColor2 = new ColorUIResource(0x50, 0x80, 0xC0);
     private static ColorUIResource primaryColor3 = new ColorUIResource(0xC0, 0xE0, 0xFF);
-    private static ColorUIResource secondaryColor1 = new ColorUIResource(0x50, 0x50, 0x50);
+    private static ColorUIResource secondaryColor1 = new ColorUIResource(0x40, 0x40, 0x40);
     private static ColorUIResource secondaryColor2 = new ColorUIResource(0x80, 0x80, 0x80);
     private static ColorUIResource secondaryColor3 = new ColorUIResource(0xE0, 0xE0, 0xE0);
 
