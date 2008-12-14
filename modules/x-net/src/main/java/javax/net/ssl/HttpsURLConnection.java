a4c6bb5e67843fb2a87f3becb4594a6f3e55bd44
Nathan Beyer <ndbeyer@apache.org>
2008-12-14 05:57:29 +0000

    Make sslSocketFactory an instance field, per spec, rearrange static methods to top of class.
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@726378 13f79535-47bb-0310-9956-ffa450edef68

--- modules/x-net/src/main/java/javax/net/ssl/HttpsURLConnection.java
+++ modules/x-net/src/main/java/javax/net/ssl/HttpsURLConnection.java
@@ -28,52 +28,12 @@ import java.security.Principal;
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
 
-/**
- * @com.intel.drl.spec_ref
- * 
- */
 public abstract class HttpsURLConnection extends HttpURLConnection {
 
     private static HostnameVerifier defaultHostnameVerifier = new DefaultHostnameVerifier();
 
     private static SSLSocketFactory defaultSSLSocketFactory = (SSLSocketFactory) SSLSocketFactory
             .getDefault();
-
-    protected HostnameVerifier hostnameVerifier;
-
-    private static SSLSocketFactory socketFactory;
-
-    protected HttpsURLConnection(URL url) {
-        super(url);
-        hostnameVerifier = defaultHostnameVerifier;
-        socketFactory = defaultSSLSocketFactory;
-    }
-
-    public abstract String getCipherSuite();
-
-    public abstract Certificate[] getLocalCertificates();
-
-    public abstract Certificate[] getServerCertificates()
-            throws SSLPeerUnverifiedException;
-
-    public Principal getPeerPrincipal() throws SSLPeerUnverifiedException {
-        Certificate[] certs = getServerCertificates();
-        if (certs == null || certs.length == 0 || 
-        		(!(certs[0] instanceof X509Certificate))) {
-            throw new SSLPeerUnverifiedException(
-                    "No server's end-entity certificate");
-        }
-        return ((X509Certificate) certs[0]).getSubjectX500Principal();
-    }
-
-    public Principal getLocalPrincipal() {
-        Certificate[] certs = getLocalCertificates();
-        if (certs == null || certs.length == 0
-                || (!(certs[0] instanceof X509Certificate))) {
-            return null;
-        }
-        return ((X509Certificate) certs[0]).getSubjectX500Principal();
-    }
 
     public static void setDefaultHostnameVerifier(HostnameVerifier v) {
         if (v == null) {
@@ -84,17 +44,6 @@ public abstract class HttpsURLConnection extends HttpURLConnection {
 
     public static HostnameVerifier getDefaultHostnameVerifier() {
         return defaultHostnameVerifier;
-    }
-
-    public void setHostnameVerifier(HostnameVerifier v) {
-        if (v == null) {
-            throw new IllegalArgumentException("HostnameVerifier is null");
-        }
-        hostnameVerifier = v;
-    }
-
-    public HostnameVerifier getHostnameVerifier() {
-        return hostnameVerifier;
     }
 
     public static void setDefaultSSLSocketFactory(SSLSocketFactory sf) {
@@ -108,15 +57,58 @@ public abstract class HttpsURLConnection extends HttpURLConnection {
         return defaultSSLSocketFactory;
     }
 
+    protected HostnameVerifier hostnameVerifier;
+
+    private SSLSocketFactory sslSocketFactory;
+
+    protected HttpsURLConnection(URL url) {
+        super(url);
+        hostnameVerifier = defaultHostnameVerifier;
+        sslSocketFactory = defaultSSLSocketFactory;
+    }
+
+    public abstract String getCipherSuite();
+
+    public abstract Certificate[] getLocalCertificates();
+
+    public abstract Certificate[] getServerCertificates() throws SSLPeerUnverifiedException;
+
+    public Principal getPeerPrincipal() throws SSLPeerUnverifiedException {
+        Certificate[] certs = getServerCertificates();
+        if (certs == null || certs.length == 0 || (!(certs[0] instanceof X509Certificate))) {
+            throw new SSLPeerUnverifiedException("No server's end-entity certificate");
+        }
+        return ((X509Certificate) certs[0]).getSubjectX500Principal();
+    }
+
+    public Principal getLocalPrincipal() {
+        Certificate[] certs = getLocalCertificates();
+        if (certs == null || certs.length == 0 || (!(certs[0] instanceof X509Certificate))) {
+            return null;
+        }
+        return ((X509Certificate) certs[0]).getSubjectX500Principal();
+    }
+
+    public void setHostnameVerifier(HostnameVerifier v) {
+        if (v == null) {
+            throw new IllegalArgumentException("HostnameVerifier is null");
+        }
+        hostnameVerifier = v;
+    }
+
+    public HostnameVerifier getHostnameVerifier() {
+        return hostnameVerifier;
+    }
+
     public void setSSLSocketFactory(SSLSocketFactory sf) {
         if (sf == null) {
             throw new IllegalArgumentException("SSLSocketFactory is null");
         }
-        socketFactory = sf;
+        sslSocketFactory = sf;
     }
 
     public SSLSocketFactory getSSLSocketFactory() {
-        return socketFactory;
+        return sslSocketFactory;
     }
 
 }
