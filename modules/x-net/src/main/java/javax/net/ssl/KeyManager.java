e0964b07d02453dc638c95797ea3583537105408
Nathan Beyer <ndbeyer@apache.org>
2008-12-14 22:36:04 +0000

    Reformat code to make it more readable, remove unnecessary comments
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@726566 13f79535-47bb-0310-9956-ffa450edef68

--- modules/x-net/src/main/java/javax/net/ssl/KeyManager.java
+++ modules/x-net/src/main/java/javax/net/ssl/KeyManager.java
@@ -15,16 +15,7 @@
  *  limitations under the License.
  */
 
-/**
-* @author Vera Y. Petrashkova
-* @version $Revision$
-*/
-
 package javax.net.ssl;
 
-/**
- * @com.intel.drl.spec_ref
- * 
- */
 public interface KeyManager {
 }
