802a24afb91dc8cf5a5f681aa286c275b3735960
Xu Rui Zhi <regisxu@apache.org>
2009-04-27 04:31:13 +0000

    fix package name in Unix/WinSocketTest
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@768865 13f79535-47bb-0310-9956-ffa450edef68

--- modules/luni/src/test/api/windows/org/apache/harmony/luni/tests/java/net/WinSocketTest.java
+++ modules/luni/src/test/api/windows/org/apache/harmony/luni/tests/java/net/WinSocketTest.java
@@ -15,7 +15,7 @@
  *  limitations under the License.
  */
 
-package org.apache.harmony.luni.tests.java.io;
+package org.apache.harmony.luni.tests.java.net;
 
 import java.io.IOException;
 import java.io.InputStream;
