a3685037b183950f80e1582383ac8fee5935d5e8
Nathan Beyer <ndbeyer@apache.org>
2009-05-26 03:11:37 +0000

    Apply patches for HARMONY-6212 - [classlib][luni] StringBuilder behavior affects the string got by the toString
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@778555 13f79535-47bb-0310-9956-ffa450edef68

--- modules/luni/src/main/java/java/lang/AbstractStringBuilder.java
+++ modules/luni/src/main/java/java/lang/AbstractStringBuilder.java
@@ -205,7 +205,7 @@ abstract class AbstractStringBuilder {
             }
             if (end > start) {
                 int length = count - end;
-                if (length > 0) {
+                if (length >= 0) {
                     if (!shared) {
                         System.arraycopy(value, end, value, start, length);
                     } else {
