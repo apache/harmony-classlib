3cf65c94a78d363ff8cb4370047f6fe4d1f393bc
Xu Rui Zhi <regisxu@apache.org>
2009-09-21 06:16:24 +0000

    add missing @Override in SelectionKeyImpl.java
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@817171 13f79535-47bb-0310-9956-ffa450edef68

--- modules/nio/src/main/java/common/org/apache/harmony/nio/internal/SelectionKeyImpl.java
+++ modules/nio/src/main/java/common/org/apache/harmony/nio/internal/SelectionKeyImpl.java
@@ -49,10 +49,12 @@ final class SelectionKeyImpl extends AbstractSelectionKey {
         attach(attachment);
     }
 
+    @Override
     public SelectableChannel channel() {
         return channel;
     }
 
+    @Override
     public int interestOps() {
         checkValid();
         synchronized (selector.keysLock) {
@@ -60,6 +62,7 @@ final class SelectionKeyImpl extends AbstractSelectionKey {
         }
     }
 
+    @Override
     public SelectionKey interestOps(int operations) {
         checkValid();
         if ((operations & ~(channel().validOps())) != 0) {
@@ -72,11 +75,13 @@ final class SelectionKeyImpl extends AbstractSelectionKey {
         return this;
     }
 
+    @Override
     public int readyOps() {
         checkValid();
         return readyOps;
     }
 
+    @Override
     public Selector selector() {
         return selector;
     }
