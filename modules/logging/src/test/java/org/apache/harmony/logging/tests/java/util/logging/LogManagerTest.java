1b2f8ee14c4954596412ba8ed653ca80eaeb2baf
Jesse Wilson <jessewilson@apache.org>
2009-11-06 18:01:33 +0000

    Fixing loggers to load handlers eagerly and track them with a CopyOnWriteArrayList. This improves performance and fixes a bug in our current implementation. A test for the bug is included; performance metrics are available in this JIRA:   https://issues.apache.org/jira/browse/HARMONY-6362 The change to CopyOnWriteArrayList also fixes some concurrency problems. Finally, I've addressed some minor readability issues. I intend to separate style from substance in forthcoming CLs...
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@833502 13f79535-47bb-0310-9956-ffa450edef68

--- modules/logging/src/test/java/org/apache/harmony/logging/tests/java/util/logging/LogManagerTest.java
+++ modules/logging/src/test/java/org/apache/harmony/logging/tests/java/util/logging/LogManagerTest.java
@@ -599,6 +599,39 @@ public class LogManagerTest extends TestCase {
 		stream.close();
 	}
 
+    public void testReadConfigurationUpdatesRootLoggersHandlers()
+            throws IOException {
+        Properties properties = new Properties();
+        LogManager.getLogManager().readConfiguration(
+                EnvironmentHelper.PropertiesToInputStream(properties));
+
+        Logger root = Logger.getLogger("");
+        assertEquals(0, root.getHandlers().length);
+
+        properties.put("handlers", "java.util.logging.ConsoleHandler");
+        LogManager.getLogManager().readConfiguration(
+                EnvironmentHelper.PropertiesToInputStream(properties));
+
+        assertEquals(1, root.getHandlers().length);
+    }
+
+    public void testReadConfigurationDoesNotUpdateOtherLoggers()
+            throws IOException {
+        Properties properties = new Properties();
+        LogManager.getLogManager().readConfiguration(
+                EnvironmentHelper.PropertiesToInputStream(properties));
+
+        Logger logger = Logger.getLogger("testReadConfigurationDoesNotUpdateOtherLoggers");
+        assertEquals(0, logger.getHandlers().length);
+
+        properties.put("testReadConfigurationDoesNotUpdateOtherLoggers.handlers",
+                "java.util.logging.ConsoleHandler");
+        LogManager.getLogManager().readConfiguration(
+                EnvironmentHelper.PropertiesToInputStream(properties));
+
+        assertEquals(0, logger.getHandlers().length);
+    }
+
 	public void testAddRemovePropertyChangeListener() throws Exception {
 		MockPropertyChangeListener listener1 = new MockPropertyChangeListener();
 		MockPropertyChangeListener listener2 = new MockPropertyChangeListener();
