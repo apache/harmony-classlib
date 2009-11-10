62800d6e657cdf19eef7b21e6e8bda7f43a4ad6a
Jesse Wilson <jessewilson@apache.org>
2009-11-10 03:12:06 +0000

    A bunch of new tests for OutputStreams.
    This uses composition to create many tests (86 tests in this case) with few lines of code. This is most useful for things like OutputStream and Collection where there are multiple implementations that satisfy the same core behaviour. I intend to eventually create a similar set of testers for InputStreams, Readers and Writers. The main advantage of this style of test is that it becomes easy to check that all implementations conform to a specified behaviour.
    
    The motivation for this specific set is a bug in FilteredStream.flush(), where we are ignoring an exception thrown by an underlying stream. These tests fail! There should be 7 new failures. I intend to commit a change that has the fix tomorrow.
    
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@834317 13f79535-47bb-0310-9956-ffa450edef68

---
+++ support/src/test/java/org/apache/harmony/testframework/WrapperTester.java
@@ -0,0 +1,135 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.apache.harmony.testframework;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Tests behaviour common to wrapping and filtering implementations of {@link
+ * OutputStream}.
+ */
+public abstract class WrapperTester {
+
+    /**
+     * Creates a new output stream that receives one stream of bytes, optionally
+     * transforms it, and emits another stream of bytes to {@code delegate}.
+     */
+    public abstract OutputStream create(OutputStream delegate) throws Exception;
+
+    /**
+     * Decodes the bytes received by the delegate into their original form: the
+     * bytes originally received by this wrapper.
+     */
+    public abstract byte[] decode(byte[] delegateBytes) throws Exception;
+
+    public final TestSuite createTests() {
+        TestSuite result = new TestSuite();
+        result.addTest(new WrapperSinkTester().createTests());
+        result.addTest(new WrapperTestCase("wrapperTestFlushThrowsViaFlush"));
+        result.addTest(new WrapperTestCase("wrapperTestFlushThrowsViaClose"));
+        return result;
+    }
+
+    @Override public String toString() {
+        return getClass().getName();
+    }
+
+    private class WrapperSinkTester extends SinkTester {
+        private ByteArrayOutputStream delegate;
+
+        @Override public OutputStream create() throws Exception {
+            delegate = new ByteArrayOutputStream();
+            return WrapperTester.this.create(delegate);
+        }
+
+        @Override public byte[] getBytes() throws Exception {
+            return WrapperTester.this.decode(delegate.toByteArray());
+        }
+
+        @Override public String toString() {
+            return WrapperTester.this.toString();
+        }
+    }
+
+    public class WrapperTestCase extends TestCase {
+
+        private WrapperTestCase(String name) {
+            super(name);
+        }
+
+        public void wrapperTestFlushThrowsViaFlush() throws Exception {
+            FailOnFlushOutputStream delegate = new FailOnFlushOutputStream();
+
+            OutputStream o = create(delegate);
+            try {
+                // any of these is permitted to flush
+                o.write(new byte[] { 8, 6, 7, 5 });
+                o.write(new byte[] { 3, 0, 9 });
+                o.flush();
+                assertTrue(delegate.flushed);
+                fail("flush exception ignored");
+            } catch (IOException expected) {
+                assertEquals("Flush failed" , expected.getMessage());
+            }
+        }
+
+        public void wrapperTestFlushThrowsViaClose() throws Exception {
+            FailOnFlushOutputStream delegate = new FailOnFlushOutputStream();
+
+            OutputStream o = create(delegate);
+            try {
+                // any of these is permitted to flush
+                o.write(new byte[] { 8, 6, 7, 5 });
+                o.write(new byte[] { 3, 0, 9 });
+                o.close();
+                assertTrue(delegate.flushed);
+                fail("flush exception ignored");
+            } catch (IOException expected) {
+                assertEquals("Flush failed" , expected.getMessage());
+            }
+        }
+
+        // adding a new test? Don't forget to update createTests().
+
+        @Override public String getName() {
+            return WrapperTester.this.toString() + ":" + super.getName();
+        }
+
+        private class FailOnFlushOutputStream extends ByteArrayOutputStream {
+            boolean flushed = false;
+
+            @Override public void close() throws IOException {
+                flush();
+                super.close();
+            }
+
+            @Override public void flush() throws IOException {
+                if (!flushed) {
+                    flushed = true;
+                    throw new IOException("Flush failed");
+                }
+                super.flush();
+            }
+        }
+    }
+}
