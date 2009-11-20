1ca4f88409c052ee9bbea2c511c7c845d0ba39d8
Jesse Wilson <jessewilson@apache.org>
2009-11-20 02:01:43 +0000

    Changing timers so timer threads die if ever timer tasks throw unchecked exceptions. Previously we kept the timer alive (not consistent with RI) and suppressed an exception (bad user experience). With this change, once a timer task fails, its timer will no longer accept new tasks.
    git-svn-id: https://svn.apache.org/repos/asf/harmony/enhanced/classlib/trunk@882385 13f79535-47bb-0310-9956-ffa450edef68

--- modules/luni/src/main/java/java/util/Timer.java
+++ modules/luni/src/main/java/java/util/Timer.java
@@ -217,7 +217,7 @@ public class Timer {
                         // no tasks scheduled -- sleep until any task appear
                         try {
                             this.wait();
-                        } catch (InterruptedException e) {
+                        } catch (InterruptedException ignored) {
                         }
                         continue;
                     }
@@ -241,8 +241,7 @@ public class Timer {
                         // sleep!
                         try {
                             this.wait(timeToSleep);
-                        } catch (InterruptedException e) {
-                            // Ignored
+                        } catch (InterruptedException ignored) {
                         }
                         continue;
                     }
@@ -285,11 +284,16 @@ public class Timer {
                     }
                 }
 
-                // run the task
+                boolean taskCompletedNormally = false;
                 try {
                     task.run();
-                } catch (Exception e) {
-                    // Ignored
+                    taskCompletedNormally = true;
+                } finally {
+                    if (!taskCompletedNormally) {
+                        synchronized (this) {
+                            cancelled = true;
+                        }
+                    }
                 }
             }
         }
